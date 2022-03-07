d5dc8018624c49eff47bdfabd5834b039a1377ae
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-03-07 22:10:59 +0100

    Create a new window, render footer and header, and add info

---
+++ src/calculator.rs
@@ -0,0 +1,160 @@
+use eframe::{
+	egui::{
+		self, Button, Color32, Context, FontData, FontDefinitions, FontFamily, Hyperlink, Label, Layout, RichText, TextureHandle, TopBottomPanel, Ui, Window,
+		TextStyle::{Body, Heading, Monospace}
+	}
+};
+use std::{
+	collections::HashMap,
+	time::{SystemTime, UNIX_EPOCH}
+};
+
+const MONOSPACE_FONT_BYTES: &[u8] = include_bytes!("../assets/fonts/monospace_font.ttf");
+const REGULAR_FONT_BYTES: &[u8] = include_bytes!("../assets/fonts/regular_font.otf");
+const PADDING: f32 = 5.0;
+
+pub struct Values {
+	general_messages: i64,
+	counting_messages: i64,
+	secret_area: bool,
+	negacies_converted: i64
+}
+
+pub struct Calculator {
+	pub textures: HashMap<String, TextureHandle>,
+	pub textures_loaded: bool,
+	pub info_active: bool,
+	build_date: Vec<String>,
+	version: String,
+	general_legacies: Vec<i64>,
+	counting_legacies: Vec<i64>,
+	secret_area_cost: i64
+}
+
+impl Calculator {
+	pub fn new(build_date: Vec<String>, version: String, general_legacies: Vec<i64>, counting_legacies: Vec<i64>, secret_area_cost: i64) -> Self {
+		Self {
+			textures: HashMap::new(),
+			textures_loaded: false,
+			info_active: false,
+			build_date,
+			version,
+			general_legacies,
+			counting_legacies,
+			secret_area_cost
+		}
+	}
+
+	pub fn initialise_fonts(&self, ctx: &Context) {
+		let mut fonts = FontDefinitions::default();
+        fonts.font_data.insert("monospace".to_owned(), FontData::from_static(MONOSPACE_FONT_BYTES));
+        fonts.font_data.insert("regular".to_owned(), FontData::from_static(REGULAR_FONT_BYTES));
+		fonts.families.get_mut(&FontFamily::Monospace).unwrap()
+    		.insert(0, "monospace".to_owned());
+		fonts.families.get_mut(&FontFamily::Proportional).unwrap()
+			.insert(0, "regular".to_owned());
+		fonts.families.get_mut(&FontFamily::Proportional).unwrap()
+			.push("monospace".to_owned());
+		fonts.families.get_mut(&FontFamily::Monospace).unwrap()
+			.push("regular".to_owned());
+        ctx.set_fonts(fonts);
+
+		let mut customized_text_styles = ctx.style().text_styles.clone();
+
+		customized_text_styles.insert(
+		Body, egui::FontId::new(16.0, FontFamily::Proportional)
+		);
+		customized_text_styles.insert(
+		Heading, egui::FontId::new(24.0, FontFamily::Proportional)
+		);
+		ctx.set_style(egui::Style {
+			text_styles: customized_text_styles,
+			..Default::default()
+		});
+	}
+
+	pub fn initialise_textures(&mut self, ctx: &egui::Context, images: Vec<&[u8]>, names: Vec<String>, sizes: Vec<[usize; 2]>) {
+		for i in 0..images.len() {
+			let pixels = images[i];
+			let name = &names[i];
+			let mut texture: Option<TextureHandle> = None;
+			let tx: &TextureHandle = texture.get_or_insert_with(|| {
+				ctx.load_texture(name, egui::ColorImage::from_rgba_unmultiplied(sizes[i], pixels))
+			});
+			self.textures.insert(name.to_owned(), tx.to_owned());
+		}
+		self.textures_loaded = true;
+	}
+
+	pub fn render_footer(&self, ctx: &Context) {
+		TopBottomPanel::bottom("footer").show(ctx, |ui| {
+			ui.vertical_centered(|ui| {
+				ui.add_space(PADDING);
+				ui.add(Hyperlink::from_label_and_url(RichText::new("App source code | Made by Pandicon").text_style(Monospace), "https://github.com/Pandicon/Alias-Heaven-Calculator"));
+				ui.add_space(PADDING);
+			});
+		});
+	}
+
+	pub fn render_top_panel(&mut self, ctx: &Context) {
+		TopBottomPanel::top("top_panel").frame(self.default_frame()).show(ctx, |ui| {
+            ui.add_space(PADDING);
+			egui::menu::bar(ui, |ui| {
+				ui.with_layout(Layout::left_to_right(), |ui| {
+					ui.add(Label::new(RichText::new(format!("{}", self.name())).text_style(Heading)));
+				});
+				ui.with_layout(Layout::right_to_left(), |ui| {
+					let info_btn = ui.add(Button::new(RichText::new("ℹ").text_style(Body)));
+					if info_btn.clicked() {
+						self.info_active = true;
+					}
+				});
+			});
+            ui.add_space(PADDING);
+		});
+	}
+
+	pub fn name(&self) -> &str {
+		"Alias Heaven Calculator"
+	}
+
+	pub fn render_info(&mut self, ctx: &Context) {
+		Window::new("Information").show(ctx, |ui| {
+			ui.label(RichText::new("App information").text_style(Heading).size(20.0));
+			ui.horizontal(|ui| {
+				ui.label("App by:");
+				ui.add(Hyperlink::from_label_and_url("Pandicon", "https://github.com/Pandicon"));
+			});
+			ui.horizontal(|ui| {
+				ui.label("Game source code:");
+				ui.add(Hyperlink::from_label_and_url("https://github.com/Pandicon/Alias-Heaven-Calculator", "https://github.com/Pandicon/Alias-Heaven-Calculator"));
+			});
+			ui.label("");
+			ui.label(RichText::new("Build information").text_style(Heading).size(20.0));
+			ui.label(format!("Version: {}", self.version));
+			let bd = &self.build_date;
+			ui.label(format!("Built on {}/{}/{} at {}:{}:{} UTC", bd[2], bd[1], bd[0], bd[3], bd[4], bd[5]));
+			ui.horizontal(|ui| {
+				let close_btn = ui.add(Button::new(RichText::new("Close").text_style(Body)));
+				if close_btn.clicked() {
+					self.info_active = false;
+				}
+			});
+		});
+	}
+	
+	pub fn render_window(&mut self, ui: &mut Ui, initial_h: f32) {
+		
+	}
+
+	pub fn default_frame(&self) -> egui::Frame {
+		egui::Frame::default().stroke(egui::Stroke::new(2.0, Color32::from_rgb(43, 43, 43))).margin(egui::style::Margin::symmetric(7.0, 2.0)).fill(Color32::from_rgb(27, 27, 27))
+	}
+}
+
+fn get_unix() -> u128 {
+	let start = SystemTime::now();
+    start.duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+		.as_millis()
+}
---
+++ src/main.rs
@@ -0,0 +1,71 @@
+include!(concat!(env!("OUT_DIR"), "/const_gen.rs"));
+
+#[path = "./calculator.rs"] mod calculator;
+
+use calculator::Calculator;
+use dotenv::dotenv;
+use eframe::{
+    emath::Vec2,
+    egui::{
+        self, CentralPanel, ScrollArea
+    },
+    epi::{self, App, IconData, NativeOptions},
+    run_native
+};
+use std::env;
+use winconsole;
+
+const W: f32 = 540.0;
+const H: f32 = 900.0;
+
+impl App for Calculator {
+    fn setup(&mut self, ctx: &egui::Context, _frame: &epi::Frame, _storage: Option<&dyn epi::Storage>) {
+        self.initialise_fonts(&ctx);
+    }
+
+    fn update(&mut self, ctx: &egui::Context, _frame: &epi::Frame) {
+        if self.info_active {
+            self.render_info(&ctx);
+        }
+        self.render_top_panel(ctx);
+        CentralPanel::default().show(ctx, |ui| {
+            ScrollArea::new([true, false]).show(ui, |mut ui| {
+                self.render_window(&mut ui, H);
+            });
+            self.render_footer(ctx);
+        });
+        ctx.request_repaint();
+    }
+
+    fn name(&self) -> &str {
+        self.name()
+    }
+}
+
+fn main() {
+    dotenv().ok();
+    if let Err(_) = env::var("RUST_LOG") {
+        env::set_var("RUST_LOG", "INFO");
+    }
+    match env::var("LOGS") {
+        Ok(val) => if val.to_lowercase() != "on" {
+            winconsole::window::hide();
+        },
+        Err(_) => winconsole::window::hide()
+    }
+    tracing_subscriber::fmt::init();
+
+    let icon_rgba: Vec<u8> = ICON_RGBA.iter().cloned().collect();
+    let build_date = BUILD_DATE.iter().cloned().map(|val| val.to_string()).collect();
+    let version = VERSION.to_string();
+    let general_legacies = GENERAL_LEGACIES.iter().cloned().collect();
+    let counting_legacies = COUNTING_LEGACIES.iter().cloned().collect();
+    println!("{:?}", general_legacies);
+
+    let app = Calculator::new(build_date, version, general_legacies, counting_legacies, SECRET_AREA_COST);
+    let mut window_options = NativeOptions::default();
+    window_options.icon_data = Some(IconData {rgba: icon_rgba, width: ICON_WIDTH, height: ICON_HEIGHT});
+    window_options.initial_window_size = Some(Vec2::new(W, H));
+    window_options.resizable = false;
+    run_native(Box::new(app), window_options);
+}
